75ad97dc57a74d4b088b9993732ad08407a3e089
Denis167518 <denis167518@gmail.com>
2020-08-13 21:40:36 +0300

    Updates

---
+++ precache-manifest.85146708d3537efb528ce8b124eec05a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98551a0d0261dd7e69b66cceaf7acea4",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "128b05e919462aa52571",
+    "url": "/books/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "d22c3081955424acbc45",
+    "url": "/books/static/css/main.e7cff403.chunk.css"
+  },
+  {
+    "revision": "128b05e919462aa52571",
+    "url": "/books/static/js/2.8d255e09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/books/static/js/2.8d255e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22c3081955424acbc45",
+    "url": "/books/static/js/main.355198d6.chunk.js"
+  },
+  {
+    "revision": "1c3c72831bb03b6ab714",
+    "url": "/books/static/js/runtime-main.462624b3.js"
+  },
+  {
+    "revision": "14da485c0f6675f3f2ae57691b1cb0f3",
+    "url": "/books/static/media/girl-blond.14da485c.png"
+  },
+  {
+    "revision": "4c19fa3f1ce988c72774e7d45f1af9ab",
+    "url": "/books/static/media/girl-brunette.4c19fa3f.png"
+  },
+  {
+    "revision": "5181ed063f0d0488355540688c8d0e6c",
+    "url": "/books/static/media/usa.5181ed06.png"
+  }
+]);
